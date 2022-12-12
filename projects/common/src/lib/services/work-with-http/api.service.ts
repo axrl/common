c813c924f95e32299abfd2b1a3a88f41f243cf3b
Axrl <axrl@mail.ru>
2022-12-12 18:02:42 +0300

    Изменена логика работы с обновлением индикатора загрузки

--- projects/common/src/lib/services/work-with-http/api.service.ts
+++ projects/common/src/lib/services/work-with-http/api.service.ts
@@ -2,9 +2,10 @@ import { Inject, Injectable, InjectionToken } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { makeHttpParams } from './params-and-header';
 import type { ParamsAndHeaders } from './params-and-header';
-import { map, catchError, throwError, BehaviorSubject, exhaustMap } from 'rxjs';
+import { map, catchError, throwError, BehaviorSubject, of, switchMap } from 'rxjs';
 import type { Observable, } from 'rxjs';
 import { SnackService } from '../snack.service';
+import { isValue } from '../../is-value';
 
 const noTransFormIncomingDataFn = <T>(res: unknown): T => res as T;
 
@@ -102,77 +103,38 @@ export class ApiService {
       params: makeHttpParams(options?.params!, {}),
       responseType: options.responseType ?? 'json'
     };
-
-    return this._isLoadingResults.pipe(
-      exhaustMap(
-        loading => {
+    const loading = this._isLoadingResults.value;
+    return of(null).pipe(
+      switchMap(
+        () => {
           loading.increment();
           this.updateLoadingIndicator(loading);
           return options.responseType === 'text' ?
-            this.http.get(url, { ...config, responseType: options.responseType }).pipe(
-              map(
-                res => {
-                  loading.decrement();
-                  this.updateLoadingIndicator(loading);
-                  return res;
-                }),
-              catchError(
-                (message: string) => {
-                  this.snack.showText(message, true);
-                  loading.decrement();
-                  this.updateLoadingIndicator(loading);
-                  return throwError(() => message);
-                })
-            ) :
+            this.http.get(url, { ...config, responseType: options.responseType }) :
             options.responseType === 'arraybuffer' ?
-              this.http.get(url, { ...config, responseType: 'arraybuffer' }).pipe(
-                map(
-                  res => {
-                    loading.decrement();
-                    this.updateLoadingIndicator(loading);
-                    return res;
-                  }),
-                catchError(
-                  (message: string) => {
-                    this.snack.showText(message, true);
-                    loading.decrement();
-                    this.updateLoadingIndicator(loading);
-                    return throwError(() => message);
-                  })
-              ) :
+              this.http.get(url, { ...config, responseType: 'arraybuffer' }) :
               options.responseType === 'blob' ?
-                this.http.get(url, { ...config, responseType: 'blob' }).pipe(
-                  map(
-                    res => {
-                      loading.decrement();
-                      this.updateLoadingIndicator(loading);
-                      return res;
-                    }),
-                  catchError(
-                    (message: string) => {
-                      this.snack.showText(message, true);
-                      loading.decrement();
-                      this.updateLoadingIndicator(loading);
-                      return throwError(() => message);
-                    })
-                ) :
-                this.http.get<T>(url, { ...config, responseType: 'json' }).pipe(
-                  map(
-                    res => {
-                      loading.decrement();
-                      this.updateLoadingIndicator(loading);
-                      return this.methodGetMapFn<T>(res);
-                    }),
-                  catchError(
-                    (message: string) => {
-                      this.snack.showText(message, true);
-                      loading.decrement();
-                      this.updateLoadingIndicator(loading);
-                      return throwError(() => message);
-                    })
-                );
+                this.http.get(url, { ...config, responseType: 'blob' }) :
+                this.http.get<T>(url, { ...config, responseType: 'json' });
+        }),
+      map(
+        res => {
+          loading.decrement();
+          this.updateLoadingIndicator(loading);
+          return isValue(options.responseType) && ['text', 'arraybuffer', 'blob'].includes(options.responseType) ?
+            res :
+            this.methodGetMapFn<T>(res);
+        }),
+      catchError(
+        (message: string) => {
+          this.snack.showText(message, true);
+          loading.decrement();
+          this.updateLoadingIndicator(loading);
+          return throwError(() => message);
         })
-    );
+
+
+    )
   }
 
   getBlobData(url: string, params: { [param: string]: string | any; } = {}, headers?: { [header: string]: string; }) {
@@ -196,75 +158,33 @@ export class ApiService {
       params: makeHttpParams(options?.params!, {}),
       responseType: options.responseType ?? 'json'
     };
+    const loading = this._isLoadingResults.value;
 
-    return this._isLoadingResults.pipe(
-      exhaustMap(
-        loading => {
+    return of(null).pipe(
+      switchMap(
+        () => {
           loading.increment();
           this.updateLoadingIndicator(loading);
           return options.responseType === 'text' ?
-            this.http.post(url, body, { ...config, responseType: 'text' }).pipe(
-              map(
-                res => {
-                  loading.decrement();
-                  this.updateLoadingIndicator(loading);
-                  return res;
-                }),
-              catchError(
-                (message: string) => {
-                  this.snack.showText(message, true);
-                  loading.decrement();
-                  this.updateLoadingIndicator(loading);
-                  return throwError(() => message);
-                })
-            ) :
+            this.http.post(url, body, { ...config, responseType: 'text' }) :
             options.responseType === 'arraybuffer' ?
-              this.http.post(url, body, { ...config, responseType: 'arraybuffer' }).pipe(
-                map(
-                  res => {
-                    loading.decrement();
-                    this.updateLoadingIndicator(loading);
-                    return res;
-                  }),
-                catchError(
-                  (message: string) => {
-                    this.snack.showText(message, true);
-                    loading.decrement();
-                    this.updateLoadingIndicator(loading);
-                    return throwError(() => message);
-                  })
-              ) :
+              this.http.post(url, body, { ...config, responseType: 'arraybuffer' }) :
               options.responseType === 'blob' ?
-                this.http.post(url, body, { ...config, responseType: 'blob' }).pipe(
-                  map(
-                    res => {
-                      loading.decrement();
-                      this.updateLoadingIndicator(loading);
-                      return res;
-                    }),
-                  catchError(
-                    (message: string) => {
-                      this.snack.showText(message, true);
-                      loading.decrement();
-                      this.updateLoadingIndicator(loading);
-                      return throwError(() => message);
-                    })
-                ) :
-                this.http.post<TResponse>(url, body, { ...config, responseType: 'json' }).pipe(
-                  map(
-                    res => {
-                      loading.decrement();
-                      this.updateLoadingIndicator(loading);;
-                      return this.methodPostMapFn<TResponse>(res);
-                    }),
-                  catchError(
-                    (message: string) => {
-                      this.snack.showText(message, true);
-                      loading.decrement();
-                      this.updateLoadingIndicator(loading);;
-                      return throwError(() => message);
-                    })
-                );
+                this.http.post(url, body, { ...config, responseType: 'blob' }) :
+                this.http.post<TResponse>(url, body, { ...config, responseType: 'json' });
+        }),
+      map(
+        res => {
+          loading.decrement();
+          this.updateLoadingIndicator(loading);;
+          return this.methodPostMapFn<TResponse>(res);
+        }),
+      catchError(
+        (message: string) => {
+          this.snack.showText(message, true);
+          loading.decrement();
+          this.updateLoadingIndicator(loading);;
+          return throwError(() => message);
         })
     );
   }
@@ -302,27 +222,26 @@ export class ApiService {
     if (options && options.params) {
       options.params = makeHttpParams(options.params, {});
     };
-
-    return this._isLoadingResults.pipe(
-      exhaustMap(
-        loading => {
+    const loading = this._isLoadingResults.value;
+    return of(null).pipe(
+      switchMap(
+        () => {
           loading.increment();
           this.updateLoadingIndicator(loading);
-          return this.http.put<TResponse>(url, body, options).pipe(
-            map(
-              res => {
-                loading.decrement();
-                this.updateLoadingIndicator(loading);;
-                return this.methodPutMapFn<TResponse>(res);
-              }),
-            catchError(
-              (message: string) => {
-                this.snack.showText(message, true);
-                loading.decrement();
-                this.updateLoadingIndicator(loading);;
-                return throwError(() => message);
-              })
-          );
+          return this.http.put<TResponse>(url, body, options);
+        }),
+      map(
+        res => {
+          loading.decrement();
+          this.updateLoadingIndicator(loading);;
+          return this.methodPutMapFn<TResponse>(res);
+        }),
+      catchError(
+        (message: string) => {
+          this.snack.showText(message, true);
+          loading.decrement();
+          this.updateLoadingIndicator(loading);;
+          return throwError(() => message);
         })
     );
   }
@@ -330,27 +249,27 @@ export class ApiService {
   delete<T>(url: string,
     params: { [param: string]: string | any; }
       = {}): Observable<T> {
+    const loading = this._isLoadingResults.value;
 
-    return this._isLoadingResults.pipe(
-      exhaustMap(
-        loading => {
+    return of(null).pipe(
+      switchMap(
+        () => {
           loading.increment();
           this.updateLoadingIndicator(loading);
-          return this.http.delete<T>(url, { params: makeHttpParams(params, {}) }).pipe(
-            map(
-              res => {
-                loading.decrement();
-                this.updateLoadingIndicator(loading);
-                return this.methodDeleteMapFn<T>(res);
-              }),
-            catchError(
-              (message: string) => {
-                this.snack.showText(message, true);
-                loading.decrement();
-                this.updateLoadingIndicator(loading);;
-                return throwError(() => message);
-              })
-          );
+          return this.http.delete<T>(url, { params: makeHttpParams(params, {}) });
+        }),
+      map(
+        res => {
+          loading.decrement();
+          this.updateLoadingIndicator(loading);
+          return this.methodDeleteMapFn<T>(res);
+        }),
+      catchError(
+        (message: string) => {
+          this.snack.showText(message, true);
+          loading.decrement();
+          this.updateLoadingIndicator(loading);
+          return throwError(() => message);
         })
     );
   }
