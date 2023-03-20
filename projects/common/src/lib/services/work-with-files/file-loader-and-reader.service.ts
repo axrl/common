de09be1d2ab832742b29f484cdad62f2794d6a3d
Ivan Larchenkov <i.larchenkov@limecreditgroup.com>
2023-03-20 21:45:35 +0700

    (common) add returnFileSize

--- projects/common/src/lib/services/work-with-files/file-loader-and-reader.service.ts
+++ projects/common/src/lib/services/work-with-files/file-loader-and-reader.service.ts
@@ -1,45 +1,61 @@
 import { Injectable } from '@angular/core';
 
+/**Форматирует размер файла из байтов в мегабайты */
+export function returnFileSize(size: number): number {
+  return +(size / 1048576).toFixed(1);
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FileLoaderAndReaderService {
+  constructor() {}
 
-  constructor() { }
+  returnFileSize = returnFileSize;
 
   async onlyLoadFile(accept: string): Promise<File> {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       let fileInput: HTMLInputElement | null = document.createElement('input');
-      [ fileInput!.type, fileInput!.accept, fileInput!.multiple ] = [ 'file', accept, false ];
+      [fileInput!.type, fileInput!.accept, fileInput!.multiple] = [
+        'file',
+        accept,
+        false,
+      ];
       const listener = (event: Event) => {
         fileInput!.removeEventListener('change', listener);
         fileInput = null;
-        const result = (<HTMLInputElement> event?.target)?.files?.[ 0 ];
+        const result = (<HTMLInputElement>event?.target)?.files?.[0];
         if (result) {
           resolve(result);
-        };
+        }
       };
       fileInput.addEventListener('change', listener);
       fileInput.click();
     });
   }
 
-  async readFileAsBase64(accept: string): Promise<{ name: string, data: string; }> {
+  async readFileAsBase64(
+    accept: string
+  ): Promise<{ name: string; data: string }> {
     const file = await this.onlyLoadFile(accept);
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
-      reader.onloadend = e => resolve({ name: file.name, data: btoa(e!.target!.result as string) });
-      reader.onerror = e => reject(e);
+      reader.onloadend = (e) =>
+        resolve({ name: file.name, data: btoa(e!.target!.result as string) });
+      reader.onerror = (e) => reject(e);
       reader.readAsBinaryString(file);
     });
   }
 
-  async readFileAsDataUrl(accept: string): Promise<{ name: string, dataUrl: string; }> {
+  async readFileAsDataUrl(
+    accept: string
+  ): Promise<{ name: string; dataUrl: string }> {
     const file = await this.onlyLoadFile(accept);
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
-      reader.onloadend = e => resolve({ name: file.name, dataUrl: e!.target!.result as string });
-      reader.onerror = e => reject(e);
+      reader.onloadend = (e) =>
+        resolve({ name: file.name, dataUrl: e!.target!.result as string });
+      reader.onerror = (e) => reject(e);
       reader.readAsDataURL(file);
     });
   }
