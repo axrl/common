af44ce0ab612e1854d0ec5449301b08ac7368c97
Ivan Larchenkov <i.larchenkov@limecreditgroup.com>
2023-08-11 19:22:43 +0300

    lint snack service

--- projects/common/src/lib/services/snack.service.ts
+++ projects/common/src/lib/services/snack.service.ts
@@ -1,31 +1,34 @@
-import { Injectable, Inject } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import type { MatSnackBarConfig } from '@angular/material/snack-bar';
-import { isValue } from '../functions';
-import { SNACK_SERVICE_CONFIG } from '../di-tokens';
-import type { SnackServiceConfig } from '../di-tokens';
+import {Inject, Injectable} from '@angular/core';
+import type {MatSnackBarConfig, MatSnackBarRef} from '@angular/material/snack-bar';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import type {SnackServiceConfig} from '../di-tokens';
+import {SNACK_SERVICE_CONFIG} from '../di-tokens';
+import {isValue} from '../functions';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class SnackService {
+    constructor(
+        private snackBar: MatSnackBar,
+        @Inject(SNACK_SERVICE_CONFIG)
+        private snackServiceConfig: SnackServiceConfig<unknown> | undefined,
+    ) {}
 
-  constructor(
-    private snackBar: MatSnackBar,
-    @Inject(SNACK_SERVICE_CONFIG) private snackServiceConfig: SnackServiceConfig<unknown> | undefined
-  ) { }
-
-  showText(message: string, isError: boolean = false, config?: MatSnackBarConfig) {
-    return isValue(this.snackServiceConfig) ?
-      this.snackBar.openFromComponent(this.snackServiceConfig.component, {
-        duration: isError ? 5000 : 2000,
-        ... this.snackServiceConfig.snackBarConfig(message, isError),
-        ...config
-      }) :
-      this.snackBar.open(message, 'OK', {
-        duration: isError ? 5000 : 2000,
-        ...config
-      });
-  }
-
+    showText(
+        message: string,
+        isError: boolean = false,
+        config?: MatSnackBarConfig,
+    ): MatSnackBarRef<unknown> {
+        return isValue(this.snackServiceConfig)
+            ? this.snackBar.openFromComponent(this.snackServiceConfig.component, {
+                  duration: isError ? 5000 : 2000,
+                  ...this.snackServiceConfig.snackBarConfig(message, isError),
+                  ...config,
+              })
+            : this.snackBar.open(message, 'OK', {
+                  duration: isError ? 5000 : 2000,
+                  ...config,
+              });
+    }
 }
